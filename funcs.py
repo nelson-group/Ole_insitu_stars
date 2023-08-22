02362be2c85440c41371a1be8ecfa2b5b4857373
Ole Wittig <olwitt@vera02.bc.rzg.mpg.de>
2023-08-22 10:57:17 +0200

    now more functions (e.g. distance), bc. insituFuncs not necessary anymore

--- funcs.py
+++ funcs.py
@@ -77,3 +77,36 @@ def areEqual(A, B):
         if (m[i] != 0):
             return False         
     return True
+
+@njit
+def dist(x,y,boxSize):
+    diff = x-y
+    diff[np.where(diff>boxSize/2)[0]] -= boxSize
+    diff[np.where(diff<=-boxSize/2)[0]] += boxSize
+    r=np.linalg.norm(diff)
+    return r
+
+def binData(val, numBins):
+    
+    minVal = min(val)
+    maxVal = max(val)
+    
+    binWidth = (maxVal - minVal) / numBins
+    
+    frac = np.zeros(numBins)
+    bins = np.linspace(minVal,maxVal,numBins)
+    
+    for b in range(numBins):
+        indices=np.where(np.logical_and(val >= minVal + b*binWidth, val < minVal + (b+1)*binWidth))[0]
+        frac[b]=indices.size      
+    return bins,frac
+
+def binData_w_bins(val, bins): 
+    num = np.zeros(bins.size)
+    for b in range(bins.size):
+        if b < bins.size-1:
+            indices = np.where(np.logical_and(val >= bins[b], val < bins[b+1]))[0]
+        else:
+            indices = np.where(val > bins[-1])[0]
+        num[b] = indices.size      
+    return num
